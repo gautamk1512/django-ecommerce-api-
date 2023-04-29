4d2bee9edda040817c8998f81700b9c4bf50d428
Gautam singh <gautamk1512>
2023-04-29 17:55:59 +0530

    commit message 6

--- ecommerse/ecommerse/settings.py
+++ ecommerse/ecommerse/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ecommerce',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Gautamabc321@',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
